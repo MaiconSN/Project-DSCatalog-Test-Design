2b51fd929c7933d968481080c6c1ec2b73a7944f
MaiconSN <maicon.joey@gmail.com>
2025-07-24 18:11:25 -0300

    POST Category

--- backend/dscatalog/src/main/java/com/myproject/dscatalog/services/CategoryService.java
+++ backend/dscatalog/src/main/java/com/myproject/dscatalog/services/CategoryService.java
@@ -29,4 +29,12 @@ public class CategoryService {
 		Category entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new CategoryDTO(entity);
 	}
+	
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+	}
 }
